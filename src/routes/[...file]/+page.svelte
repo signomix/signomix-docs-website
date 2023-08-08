8c717121ba628ad24be4025106dbed87fced3e86
Grzegorz Skorupa <g.skorupa@gmail.com>
2023-08-08 21:33:30 +0200

    wip

--- src/routes/[...file]/+page.svelte
+++ src/routes/[...file]/+page.svelte
@@ -3,30 +3,15 @@
         {#await data}
         ...
         {:then data}
+        <!-- breadcrumbs -->
         {#if data.paths!=undefined && data.paths!=null && data.paths.length>0}
         {#each data.paths as pathelem, index}
         <a href={pathelem.path}>{pathelem.name==''?'home':pathelem.name}</a> {data.paths.length-1>index?' / ':''}
         {/each}
         {/if}
         {/await}
-        <!--
-        <nav aria-label="breadcrumb" class="d-fluid">
-            <ol class="breadcrumb d-fluid">
-                {#await data}
-                ...
-                {:then data}
-                {#if data.paths!=undefined && data.paths!=null && data.paths.length>0}
-                {#each data.paths as pathelem, index}
-                <li class="breadcrumb-item"><a href={pathelem.path}>{pathelem.name==''?'home':pathelem.name}</a></li>
-                {/each}
-                {/if}
-                {/await}
-            </ol>
-        </nav>
-    -->
     </div>
 </div>
-
 <div class="row container-fluid">
     <div class="col">
         <div class="row w-100">
@@ -34,39 +19,19 @@
                 {#await data}
                 ...
                 {:then result}
-                {#if result!=undefined && result!=null && result.documents!=null && result.documents!=undefined &&
-                result.documents.length>0}
+                {#if result!=undefined && result!=null && result.documents!=null && result.documents!=undefined && result.documents.length>0}
                 {@html result.documents[0].content}
                 {:else}
-                <h1>Welcome to Cricket website example</h1>
-                <p>If you see this page, Cricket HCMS is probably unavailable at the configured address.</p>
-                <a href="/toc.md">TOC</a>
-                <a href="/test/test.md">test</a>
+                <h1>404</h1>
+                <p>Page not found.</p>
                 {/if}
                 {:catch error}
                 {error.message}
                 {/await}
             </div>
-            <!--
-            <div class="col-2 border-start border-end">
-                Tags:<br>
-                {#await data}
-                ...
-                {:then result}
-                {#if result!=undefined && result!=null && result.tags!=null}
-                {#each result.tags as tag, index}
-                {tag}
-                {/each}
-                {/if}
-                {:catch error}
-                {error.message}
-                {/await}
-            </div>
-        -->
         </div>
     </div>
 </div>
-
 <script>
     export let data
 </script>
