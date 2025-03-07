2d3ff1f90e3b32f5297b90cd1fbdf2cb59a9808d
gskorupa <g.skorupa@gmail.com>
2025-03-08 00:22:52 +0100

    Implement user session token management and update load function to utilize writable store

--- src/routes/[...file]/+page.server.js
+++ src/routes/[...file]/+page.server.js
@@ -1,28 +1,35 @@
 import { hcms } from '$lib/hcms.js';
 import { dev } from '$app/environment';
 import { goto } from '$app/navigation';
+import { token } from '$lib/usersession.js';
+import { get } from 'svelte/store';
 
 export async function load(params) {
     console.log('params.url.search', params.url.search)
-    var token = null;
+    let userToken
+    token.subscribe((value) => userToken = value)
     // Get token from URL
-    if(params.url.search!=undefined && params.url.search!=null){
+    if (params.url.search != undefined && params.url.search != null) {
         let paramsArray = null;
-        if(params.url.search.startsWith('?')){
+        if (params.url.search.startsWith('?')) {
             paramsArray = params.url.search.substring(1).split('&');
-        }else{
+        } else {
             paramsArray = params.url.search.split('&');
         }
         for (let i = 0; i < paramsArray.length; i++) {
             let param = paramsArray[i].split('=');
-            if(param[0]=='token'){
-                token = param[1];
+            if (param[0] == 'token') {
+                token.set(param[1]);
             }
         }
     }
-    console.log('token', token)
-    if(process.env.PUBLIC_ORGANIZATIONS!=undefined && process.env.PUBLIC_ORGANIZATIONS!=null && token==null){ 
-        goto('/login')
+    console.log('token', userToken)
+    if (process.env.PUBLIC_ORGANIZATIONS != undefined && process.env.PUBLIC_ORGANIZATIONS != null && userToken == null) {
+        try {
+            goto('/login')
+        } catch (err) {
+            console.log(err)
+        }
     }
     try {
         /* var organizationCode = null;
@@ -36,7 +43,7 @@ export async function load(params) {
             params,
             process.env.PUBLIC_HCMS_INDEX,
             process.env.PUBLIC_HCMS_ROOT,
-            token,
+            userToken,
             null,
             process.env.PUBLIC_HCMS_LANGUAGE,
             process.env.PUBLIC_HCMS_LANGUAGES
